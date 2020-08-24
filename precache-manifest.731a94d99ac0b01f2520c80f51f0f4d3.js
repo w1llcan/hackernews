9e5192c4e51e9819bf016fd37b053e3641fc4a44
w1llcan <willcan3358@gmail.com>
2020-08-24 14:01:01 -0400

    Updates

---
+++ precache-manifest.731a94d99ac0b01f2520c80f51f0f4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e32cea7ab9d91d5a1f2c236beef189",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "8acb40758c923b9f39ec",
+    "url": "/hackernews/static/css/main.5213b4a2.chunk.css"
+  },
+  {
+    "revision": "7b14b20cd33d52ad8bb7",
+    "url": "/hackernews/static/js/2.7a09acfb.chunk.js"
+  },
+  {
+    "revision": "8acb40758c923b9f39ec",
+    "url": "/hackernews/static/js/main.8d964afc.chunk.js"
+  },
+  {
+    "revision": "371654036c4f6eb6db7c",
+    "url": "/hackernews/static/js/runtime~main.91ad1a63.js"
+  }
+]);
